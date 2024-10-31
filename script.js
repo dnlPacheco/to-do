97a46a549bf0def4bf59b20dea5db007351cb7d8
daniel <daniel@kyte.com.br>
2024-10-31 20:32:46 -0300

    refact: move list item creation logic to a separate function

--- script.js
+++ script.js
@@ -1,69 +1,68 @@
-const localStorageKey = 'to-do-list'
+const localStorageKey = "to-do-list";
 
 function getTodoList() {
-    let tasks = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
-    return tasks
+  let tasks = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+  return tasks;
 }
 
 function saveTodoList(tasks) {
-    localStorage.setItem(localStorageKey, JSON.stringify(tasks))
+  localStorage.setItem(localStorageKey, JSON.stringify(tasks));
 }
 
 function addNewTask() {
-    const input = document.querySelector('#input-new-task')
+  const input = document.querySelector("#input-new-task");
 
-    if (!input.value) {
-       return alert('Digite algum texto!')
-    }
+  if (!input.value) {
+    return alert("Digite algum texto!");
+  }
 
-    let tasks = getTodoList()  
-    
-    tasks.push({
-        id: Date.now(),
-        task: input.value.trim()
-    })
+  let tasks = getTodoList();
 
-    saveTodoList(tasks)
+  tasks.push({
+    id: Date.now(),
+    task: input.value.trim(),
+  });
 
-    input.value = ''
+  saveTodoList(tasks);
 
-    showTodoList()
+  input.value = "";
 
+  showTodoList();
+}
+
+function createListItem(task) {
+  const listItem = document.createElement("li");
+  listItem.textContent = task?.task;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  deleteBtn.addEventListener("click", () => {
+    removeTask(task);
+  });
+  listItem.appendChild(deleteBtn);
+
+  return listItem;
 }
 
 function showTodoList() {
-    let tasks = getTodoList()
-    let todoList = document.getElementById('to-do-list')
+  let tasks = getTodoList();
+  let todoList = document.getElementById("to-do-list");
+  todoList.innerHTML = "";
 
-    todoList.innerHTML = ''
-
-    // TODO adicionar checkbox para marcar tarefa concluída
-    tasks.forEach(item => {
-        const listItem = document.createElement('li')
-        listItem.textContent = item.task
-        
-
-        const deleteBtn = document.createElement('button')
-        deleteBtn.textContent = 'Delete'
-        deleteBtn.addEventListener('click', (event) => {
-            removeTask(item)
-        })
-
-        listItem.appendChild(deleteBtn)
-        todoList.appendChild(listItem)
-
-    });
+  tasks.forEach((task) => {
+    const listItem = createListItem(task);
+    todoList.appendChild(listItem);
+  });
 }
 
-function removeTask(item) {
-    const tasks = getTodoList()
-    const taskIndex = tasks.findIndex((task) => task?.id === item?.id)
+function removeTask(task) {
+  const tasks = getTodoList();
+  const taskIndex = tasks.findIndex((t) => t?.id === task?.id);
 
-    if(taskIndex !== -1) tasks.splice(taskIndex, 1)
+  if (taskIndex !== -1) tasks.splice(taskIndex, 1);
 
-    saveTodoList(tasks)
-    showTodoList()
-
+  saveTodoList(tasks);
+  showTodoList();
 }
 
-showTodoList()
+showTodoList();
